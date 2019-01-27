7141dc5d108de8140d198fd49db44fbf1499e997
Antoine <end2endzone@gmail.com>
2019-01-27 17:00:40 -0500

    Revert "Removed copyright header from all source files."
    This reverts commit a88940ebb5bea9e0d8e5b83a4883eae39740df03.

--- libVariant/dllmain.cpp
+++ libVariant/dllmain.cpp
@@ -1,3 +1,45 @@
+//
+//  libVariant Library - v2.0 - 05/26/2017
+//  Copyright (C) 2017 Antoine Beauchamp
+//  The code & updates for the library can be found on http://end2endzone.com
+//
+// AUTHOR/LICENSE:
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 3.0 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License (LGPL-3.0) for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// DISCLAIMER:
+//  This software is furnished "as is", without technical support, and with no 
+//  warranty, express or implied, as to its usefulness for any purpose.
+//
+// PURPOSE:
+//  This libVariant library allows one to create an instance of Variant class.
+//  The Variant class allows one to create a type-safe and value safe union
+//  between all known c++ basic types. An instance of a Variant class can hold
+//  any c++ type of value and can easily convert between any type of data when
+//  required.  The class automatically handles all conversion combinations and
+//  minimizes lost of data. Typically used in design where one does not want
+//  to implement conversion of c++ types using templates in every situation.
+//  Variant instance can be used to implement a similar fashion of C# properties
+//  where property editors can edit any type of property.
+//
+// HISTORY:
+// 10/29/2016 v1.0 - Initial release.
+// 05/26/2017 v2.0 - Updated API for cleaner look & updated documentation.
+//                 - Added speed optimizations.
+//                 - Added more operators: +, -, *, /, +=, -=, *=, /=, <, >, <=, >=, == and !=
+//                 - Implemented division by zero handling.
+//
 
 
 // dllmain.cpp : Defines the entry point for the DLL application.

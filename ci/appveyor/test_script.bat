69900d519d5ce4cac059970b4ebcbac0443238da
Antoine <end2endzone@gmail.com>
2019-02-03 17:14:29 -0500

    Modified test_script.* to display a separator between tests from 'libvariant_unittest' and 'libvariant_templates_unittest'.

--- ci/appveyor/test_script.bat
+++ ci/appveyor/test_script.bat
@@ -1,12 +1,16 @@
 @echo off
 
 echo =======================================================================
-echo Testing project
+echo Testing project (1 of 2)
 echo =======================================================================
 cd /d %~dp0
 cd ..\..
 cd build\bin\Release
 libvariant_unittest.exe
+
+echo =======================================================================
+echo Testing project (2 of 2)
+echo =======================================================================
 libvariant_templates_unittest.exe
 
 ::reset error in case of test case fail
